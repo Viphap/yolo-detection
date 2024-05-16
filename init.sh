9bd4c4e006740ad2e98da38c374d5899856b804c
Xuan Truong Pham <xuantruongpham@viphap.com>
2024-05-16 11:09:43 +0700

    fix sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ ROOT_DIR=$(dirname $0)
 echo "Downloading Yolo model: $MODEL ..."
 MODELS_DIR=$ROOT_DIR/app/yolo/models
 # Check if the folder exists
-if [ ! -d "$MODELS_DIR"]; then
+if [ ! -d "$MODELS_DIR" ]; then
     mkdir -p "$MODELS_DIR"
 fi
 wget -P "$MODELS_DIR" "https://github.com/xuantruongpham/yolo-server/releases/download/resources/$MODEL.onnx"
